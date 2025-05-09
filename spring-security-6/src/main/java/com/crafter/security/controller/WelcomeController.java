6a491c9aa8c876f1992427a5339990b5c0f7bd08
anindya033 <anindya033paul@gmail.com>
2025-05-09 16:30:52 +0530

    Spring Security : Dependency added. with Basic CSRF

--- spring-security-6/src/main/java/com/crafter/security/controller/WelcomeController.java
+++ spring-security-6/src/main/java/com/crafter/security/controller/WelcomeController.java
@@ -1,8 +1,16 @@
 package com.crafter.security.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/security")
@@ -12,5 +20,37 @@ public class WelcomeController {
 	public String welcome() {
 		return "Welcome to spring security...";
 	}
+	
+	@GetMapping("/csrf")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+		
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	
+	private final List<Product> productList = new ArrayList<>();
 
+    @PostMapping("/add")
+    public String addProduct(@RequestBody Product product) {
+        productList.add(product);
+        return "Product added successfully!";
+    }
+    @GetMapping("/get")
+    public List<Product> getProduct() {
+        return productList;
+    }
+
+}
+
+class Product {
+    public int productId;
+    public String productName;
+    public double productPrice;
+
+    public Product() {}
+
+    public Product(int productId, String productName, double productPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
 }
