a5a4420e1e6b1c83736765b2321669c4eb9c436e
anindya033 <anindya033paul@gmail.com>
2025-05-10 17:34:01 +0530

    Implement basic user detail... without DB

--- spring-security-6/src/main/java/com/crafter/security/config/WebSecurityConfig.java
+++ spring-security-6/src/main/java/com/crafter/security/config/WebSecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 
@@ -15,22 +21,35 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
 
 		try {
-			
-			httpSecurity.authorizeHttpRequests(
-					req -> req.anyRequest().authenticated()
-					)
-			.formLogin(Customizer.withDefaults())
-			.httpBasic(Customizer.withDefaults())
-			.csrf(csrf-> csrf.disable());
-			//.httpBasic()
-			
-			
+
+			httpSecurity.authorizeHttpRequests(req -> req.anyRequest().authenticated())
+					.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults())
+					.csrf(csrf -> csrf.disable());
+			// .httpBasic()
+
 			return httpSecurity.build();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException("Failed to build SecurityFilterChain", e);
 		}
+
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User
+				.withUsername("user")
+				.password("{noop}1234")
+				.roles("USER").build();
+
+		UserDetails nikhil = User.withUsername("nikhil")
+
+				// {noop} is a password encoder identifier that tells Spring Security:
+				// "Do not encode this password — use it as plain text."
+				.password("{noop}nikhil").roles("USER").build();
+
+		return new InMemoryUserDetailsManager(user, nikhil);
 
 	}
 
--- spring-security-6/src/main/java/com/crafter/security/controller/WelcomeController.java
+++ spring-security-6/src/main/java/com/crafter/security/controller/WelcomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("security")
 public class WelcomeController {
 	
 	@GetMapping(value="/")
